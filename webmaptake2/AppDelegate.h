6ccffef7abc563fd92b5c855c11d0ebf517be453
Douglas Bocking <douglasbocking@douglass-mini.bigpond>
2014-08-12 15:02:33 +1000

    Initial Commit

---
+++ webmaptake2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  webmaptake2
+//
+//  Created by Douglas Bocking on 12/08/2014.
+//  Copyright (c) 2014 DouglasBocking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ webmaptake2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  webmaptake2
+//
+//  Created by Douglas Bocking on 12/08/2014.
+//  Copyright (c) 2014 DouglasBocking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
